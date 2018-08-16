ee9661b055905097f2fb6d0be8e1256325bd2fb6
Rafal Maselek <rafalmaselek@gmail.com>
2018-08-16 20:02:20 +0200

    Working version of the script for both single- and muli-file modes.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # CheckMateCalculations
 Repository for CheckMate related stuff for Master thesis
+
+#### Author: Rafal Maselek
+#### email: rafalmaselek@gmail.com
+
+## 1) auto_checkmate.sh
+
+**auto_checkmate.sh** is a bash script that enables running CheckMATE with SUSYHit for many SLHA file with a single command.
+
+#### 0. Prerequisites
+**auto_checkmate.sh** requires bash console and the following software:
+
+* CheckMATE2; available at https://checkmate.hepforge.org
+
+* SUSYHit; located in res/ folder. To install SUSYHit, extract the archive and build the programme using the `make` command.
+
+#### 1. Installation:
+**auto_checkmate** is a single-file bash scripts and does not require any installation. However user have to specify some parameters: path to CheckMATE executable, path to SUSYHit executable and CheckMATE parameters. All parameters can be set by modifying the variable
+definitions at the beginning of the script. The meaning of those parameters can be found in CheckMATE article:  https://arxiv.org/pdf/1611.09856.pdf
+
+#### 2. Runnning:
+There are two running modes for the script:
+
+* The first one allows to run CheckMATE for a single SLHA file:
+`./auto_checkmate.sh name_of_SLHA_file.slha`
+
+* The second mode allows user to run CheckMATE for many SLHA files by providing a text file with names of SLHA files, one name per row. 
+`./auto_checkmate.sh name_of_list_file.txt`  
+Such file looks in a following way:  
+
+> file1.slha  
+> file2.slha  
+> file3
+
+**NOTE:** The extension is non-mandatory.  
+**NOTE2:** Leading/following white spaces are truncated.
+
+#### 3. Folder hierarchy:
+User can specify output directory by variable called *OUTDIR*. The default behavior in many-files mode is to create a subdirectory named after the name of the list file, and in it subdirectories named after natural numbers. One subdirectory for each execution of CheckMATE.  
+The folder hierarchy looks then as follows:
+```
+.
+└── slha_list
+    ├── 1
+        ├── best_signal_regions.txt
+        ├── pythia_process.log
+        ├── result.txt
+        └── total_results.txt
+    ├── 2
+        ├── best_signal_regions.txt
+        ├── pythia_process.log
+        ├── result.txt
+        └── total_results.txt
+```
+It might be useful to use SLHA files' names to label subdirectories instead of using numbers. It can be enabled by setting `USE_FILE_NAMES='true'` at the very top of the script.  The folder hierarchy looks then as follows:  
+```
+.
+└── slha_list
+    ├── file1
+        ├── best_signal_regions.txt
+        ├── pythia_process.log
+        ├── result.txt
+        └── total_results.txt
+    ├── file2
+        ├── best_signal_regions.txt
+        ├── pythia_process.log
+        ├── result.txt
+        └── total_results.txt
+```
+
+#### 4. Output:
+Each output directory contains four files:
+* pythia_process.log -- logs of pythia with cross-sections
+* result.txt --final result of CheckMATE
+* total_results.txt -- detailed results for each of the signal regions
+* best_signal_regions.txt -- can be used to establish which signal regions are the best for theory testing
---
+++ auto_checkmate.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+#--------------------------------------#
+#---- AUTO CHECKMATE BY R. MASELEK ----#
+#--------------------------------------#
+SILENT='false' 		  #set to true to disable all script generated output to stdout
+USE_FILE_NAMES='false' #set 'true' to use file names to name output folders in multi-file mode instead of using natural numbers
+
+#---- CheckMATE basic parameters
+PYTHIA='allsusy'		#which process to simulate
+ANALYSES='atlas_1802_03158'	#which analyses to perform
+NEV='5000'			#max number of events
+OUTDIR='./results'		#where to place results (NO / AT THE END)
+INDIR='./res/example_input'	#where to look for SLHA files if a list of names is provided (NO / AT THE END)
+	
+#---- CheckMATE additional parameters
+PARAMS='-q'
+
+#---- Paths to executables
+CHECKMATE=/Users/rafalmaselek/CheckMATE-2.0.26/bin/CheckMATE
+SUSYHIT=/Users/rafalmaselek/susyhit/run
+
+##############################################################################
+inside_print()
+{
+	if ! [[ $SILENT == 'true' ]]; then
+		echo $1
+	fi
+}
+
+make_output_dir()
+{
+	if [[ ! -d "$OUTDIR" ]]; then
+		mkdir $OUTDIR
+		if [[ ! -e "$OUTDIR" || ! -d "$OUTDIR" ]]; then
+			echo "[ERROR] Could not create directory for results!"
+			exit 1
+		fi
+	fi
+}
+
+clear
+inside_print "[START]"
+#---- Working area
+FILE=`echo $1`						#input file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"	#directory of bash script
+OUTPUT_EXISTS='overwrite'				#what to do if output already exists 
+
+
+# the temp directory used, within $DIR
+# omit the -p parameter to create a temporal directory in the default location
+WORK_DIR=`mktemp -d  "$DIR/tmp"`
+
+# check if tmp dir was created
+if [[ ! -d "$WORK_DIR" ]]; then
+  echo "[ERROR] Could not create temp dir! Check if it already exists and permissions!"
+  exit 1
+else
+  inside_print "[INFO] Temporary directory created: " $WORK_DIR
+fi
+
+# deletes the temp directory
+function cleanup 
+{      
+  rm -rf "$WORK_DIR"
+  inside_print "[INFO] Deleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT #COMMENT THIS TO KEEP ALL OUTPUT FROM CHECKMATE
+
+#create results directory if it doesn't exist
+make_output_dir
+
+#---- Functions
+# run SUSYHit and copy the output to WORK_DIR
+susyhit()
+{
+	inside_print "[INFO] Running SUSYHit..."
+	local SUSYHIT_DIR=$(echo `dirname $SUSYHIT`)
+	cp $INDIR/$1 $SUSYHIT_DIR/slhaspectrum.in
+	cd $SUSYHIT_DIR
+	./$(echo `basename $SUSYHIT`)
+	cp susyhit_slha.out $WORK_DIR/$1
+	cd $DIR
+}
+
+# create a convenient folder hierarchy and move interesting files in it 
+move_results()
+{
+	cd $OUTDIR
+	pwd
+	if [[ ! -d "$1" ]]; then
+		mkdir $1
+		if [[ ! -d "$1" ]]; then
+			echo "[ERROR] Could not create subdirectory for name: " $1
+			exit 1
+		fi
+	else
+		echo "[WARNING] Results for: " $1 " will be overwritten!"
+	fi
+	cp $WORK_DIR/$1/result.txt $WORK_DIR/$1/evaluation/best_signal_regions.txt $WORK_DIR/$1/evaluation/best_signal_regions.txt \
+$WORK_DIR/$1/evaluation/total_results.txt $WORK_DIR/$1/pythia/pythia_process.log ./$1
+	cd $DIR
+}
+
+#---------------------------------#
+#---- THE MAIN PART OF SCRIPT ----#
+#---------------------------------#
+
+#---- CASE 1: User provides a single SLHA file as a parameter
+if [ ${FILE: -5} == ".slha" ]; then
+	if [[ ! -e $FILE ]]; then
+    		echo "[ERROR] SLHA file: " $FILE " doesn't exist!"
+    		exit 1
+	else 
+		NAME=$(echo `basename $FILE` | cut -d'.' -f1)
+		susyhit $NAME.slha
+		$CHECKMATE -n $NAME -pyp "p p > $PYTHIA" -a $ANALYSES -maxev $NEV -slha $WORK_DIR/$NAME.slha -oe $OUTPUT_EXISTS -od $WORK_DIR $PARAMS
+		move_results $NAME
+	fi
+else
+#---- CASE 2: User provides a single file with names of SLHA files in it
+	inside_print "[INFO] Reading names of SLHA files from: " $FILE
+	IT=1
+	OUTDIR=$OUTDIR/$(echo `basename $FILE` | cut -d'.' -f1)
+	make_output_dir
+	#---- read input file line by line to extract slha file names
+	while read -r LINE <&3 || [[ -n "$LINE" ]]; do
+		echo "[INFO] Text read from file: $LINE"
+	    if ! [[ ${LINE: -5} == ".slha" ]]; then
+			LINE=$LINE.slha
+	    fi
+	    susyhit $LINE
+	    NAME=$(echo $LINE | cut -d'.' -f1)
+	    IT_NAME=$(printf '%i' $IT )
+	    if [[ $USE_FILE_NAMES == 'true' ]]; then 
+	    	IT_NAME=$NAME
+	    	if [[ -d $OUTDIR/$NAME ]]; then
+	    		echo "[WARNING] Results for: " $NAME " will be overwritten!"
+	    	fi
+	    fi
+	    $CHECKMATE -n $IT_NAME -pyp "p p > $PYTHIA" -a $ANALYSES -maxev $NEV -slha $WORK_DIR/$LINE -oe $OUTPUT_EXISTS -od $WORK_DIR $PARAMS
+		move_results $IT_NAME
+		if ! [[ $USE_FILE_NAMES == 'true' ]]; then 
+			IT=$((IT+1)) 
+		fi
+	done 3<"$FILE"
+fi
+inside_print "[END]"
