f9b54f4b211f1a5bf32dc6424b32e203eef146bc
kazuki-sakurai <kazuki.sakurai.1225@gmail.com>
2018-08-30 14:55:41 +0200

    minigrid added

--- UML/grid/generation.py
+++ UML/grid/generation.py
@@ -3,16 +3,21 @@
 import os, sys
 import numpy as np
 
-mG_ar = np.logspace(np.log10(700.1), np.log10(3500.1), 15)
-mQ_ar = np.logspace(np.log10(500.1), np.log10(3500.1), 15)
-mQ3_ar = np.logspace(np.log10(200.1), np.log10(3500.1), 15)
+n = 15 # full grid
+#n = 5 # mini grid
+
+mG_ar = np.logspace(np.log10(700.1), np.log10(3500.1), n)
+mQ_ar = np.logspace(np.log10(500.1), np.log10(3500.1), n)
+mQ3_ar = np.logspace(np.log10(200.1), np.log10(3500.1), n)
 
 print '# mG mQ mQ3 mN1'
 for mG in mG_ar:
     for mQ in mQ_ar:
         for mQ3 in mQ3_ar:
             mmin = min([mG, mQ, mQ3])
-            nn1 = 5 + int( 10.1 * np.log(mmin/200.)/np.log(3500./200.) )
+            nn1 = 5 + int( 10.1 * np.log(mmin/200.)/np.log(3500./200.) ) # full grid
+            #nn1 = 2 + int( 5.1 * np.log(mmin/200.)/np.log(3500./200.) )  # mini grid
+
             mdiff_ar = np.logspace(np.log10(10.), np.log10(mmin), nn1)
             for mdiff in mdiff_ar:
                 mn1 = mmin - mdiff 
