0d3cfa84201933fa74d2fa9f45f2965f84fa67da
Rafal Maselek <rafalmaselek@gmail.com>
2018-08-31 15:46:23 +0200

    Spectrum is kept with results. Providing no argument won't cause crash. Help can be obtained using -h or --help flag. Results will not be overwritten in any case.

--- auto_checkmate.sh
+++ auto_checkmate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #--------------------------------------#
-#---- AUTO CHECKMATE BY R. MASELEK ----#
+#---- AUTO_CHECKMATE BY R. MASELEK ----#
 #--------------------------------------#
 SILENT='false' 		  #set to true to disable all script generated output to stdout
 USE_FILE_NAMES='false' #set 'true' to use file names to name output folders in multi-file mode instead of using natural numbers
@@ -10,8 +10,8 @@ USE_FILE_NAMES='false' #set 'true' to use file names to name output folders in m
 PYTHIA='allsusy'		#which process to simulate
 ANALYSES='atlas_1802_03158'	#which analyses to perform
 NEV='5000'			#max number of events
-OUTDIR='./results'		#where to place results (NO / AT THE END)
-INDIR='./res/example_input'	#where to look for SLHA files if a list of names is provided (NO / AT THE END)
+OUTDIR='./results'		#where to place results (NO "/"" AT THE END)
+INDIR='./res/example_input'	#where to look for SLHA files if a list of names is provided (NO "/"" AT THE END)
 	
 #---- CheckMATE additional parameters
 PARAMS='-q'
@@ -23,6 +23,8 @@ inside_print()
 		echo $1
 	fi
 }
+#---- Function for printing messages to stderr
+echoerr() { echo "$@" 1>&2; }
 
 clear
 inside_print "[START]"
@@ -46,7 +48,7 @@ make_output_dir()
 	if [[ ! -d "$OUTDIR" ]]; then
 		mkdir $OUTDIR
 		if [[ ! -e "$OUTDIR" || ! -d "$OUTDIR" ]]; then
-			echo "[ERROR] Could not create directory for results!"
+			echoerr "[ERROR] Could not create directory for results!"
 			exit 1
 		fi
 	fi
@@ -64,7 +66,7 @@ WORK_DIR=$(mktemp -d $DIR/tmp_XXXXXXXX)
 
 # check if tmp dir was created
 if [[ ! -d "$WORK_DIR" ]]; then
-  echo "[ERROR] Could not create temp dir! Close the terminal and try again!"
+  echoerr "[ERROR] Could not create temp dir! Close the terminal and try again!"
   exit 1
 else
   inside_print "[INFO] Temporary directory created:  $WORK_DIR"
@@ -75,10 +77,12 @@ function cleanup()
 {      
   rm -rf "$WORK_DIR" 
   inside_print "[INFO] Deleted temp working directory $WORK_DIR"
+  inside_print ""
 }
 
-# register the cleanup function to be called on the EXIT signal
+#---- register the cleanup function to be called on the EXIT signal
 trap cleanup EXIT #COMMENT THIS TO KEEP ALL OUTPUT FROM CHECKMATE
+
 #create results directory if it doesn't exist
 make_output_dir
 
@@ -99,46 +103,80 @@ susyhit()
 move_results()
 {
 	cd $OUTDIR
-	pwd
+	II=1
+	DEST=$1
 	if [[ ! -d "$1" ]]; then
 		mkdir $1
 		if [[ ! -d "$1" ]]; then
-			echo "[ERROR] Could not create subdirectory for name: $1"
+			echoerr "[ERROR] Could not create subdirectory for name: $1"
 			exit 1
 		fi
 	else
-		echo "[WARNING] Results for: $1 will be overwritten!"
+		while [[ -d "$1_$II" ]]; do
+			II=$((II+1))
+		done
+		mkdir $(echo "$1_$II")
+		DEST=$(echo "$1_$II")
+		echo "[WARNING] Results for: $1 will be saved as $1_$II!"
+
 	fi
+	cp $WORK_DIR/$2 $WORK_DIR/$1
 	cp $WORK_DIR/$1/result.txt $WORK_DIR/$1/evaluation/best_signal_regions.txt $WORK_DIR/$1/evaluation/best_signal_regions.txt \
-$WORK_DIR/$1/evaluation/total_results.txt $WORK_DIR/$1/pythia/pythia_process.log ./$1
+$WORK_DIR/$1/evaluation/total_results.txt $WORK_DIR/$1/pythia/pythia_process.log $WORK_DIR/$1/$2 ./$DEST
 	cd $DIR
+}
+
+# print help to stdout
+print_help()
+{
+	SILENT='true'
+	echo ""
+	echo "[HELP]"
+	echo "auto_checkmate allows running SUSYHit with CheckMATE for one or many slha files at once."
+	echo "To use it, provide as an argument a path to a single slha file, or alternatively"
+	echo "to a text file containing names of slha files (one per line, files in the same folder"
+	echo "as the list-file)."
+	echo ""
 }
 
 #---------------------------------#
 #---- THE MAIN PART OF SCRIPT ----#
 #---------------------------------#
 
-#---- CASE 1: User provides a single SLHA file as a parameter
-if [ ${FILE: -5} == ".slha" ]; then
+#---- CASE 0: User provides no arguments
+if [ $# -eq 0 ]; then
+    echoerr "[ERROR] No arguments supplied! Type ./auto_checkmate --help for help!"
+    exit 1 
+#---- CASE 1: User asks for help
+elif [[ $FILE == "-h" || $FILE == "--help" ]] ; then
+	print_help
+	exit 0
+#---- CASE 2: User provides wrong flag
+elif [ ${FILE: 0:1} == "-" ]; then
+	echoerr "[ERROR] Unknown flag! Type ./auto_checkmate --help for help!"
+	exit 1
+#---- CASE 3: User provides a single SLHA file as a parameter
+elif [ ${FILE: -5} == ".slha" ]; then
 	if [[ ! -e $FILE ]]; then
-    		echo "[ERROR] SLHA file: $FILE doesn't exist!"
+    		echoerr "[ERROR] SLHA file: $FILE doesn't exist!"
     		exit 1
 	else 
 		NAME=$(echo `basename $FILE` | cut -d'.' -f1)
+		INDIR=$(dirname $FILE)
 		susyhit $NAME.slha
 		inside_print "[INFO] SUSYHit done. Running CheckMATE..."
 		$CHECKMATE -n $NAME -pyp "p p > $PYTHIA" -a $ANALYSES -maxev $NEV -slha $WORK_DIR/$NAME.slha -oe $OUTPUT_EXISTS -od $WORK_DIR $PARAMS
-		move_results $NAME
+		move_results $NAME $NAME.slha
 	fi
 else
-#---- CASE 2: User provides a single file with names of SLHA files in it
+#---- CASE 4: User provides a single file with names of SLHA files in it
 	inside_print "[INFO] Reading names of SLHA files from: $FILE"
 	IT=1
 	OUTDIR=$OUTDIR/$(echo `basename $FILE` | cut -d'.' -f1)
 	make_output_dir
 	#---- read input file line by line to extract slha file names
 	while read -r LINE <&3 || [[ -n "$LINE" ]]; do
-		echo "[INFO] Text read from input file: $LINE"
+		inside_print "[INFO] Text read from input file: $LINE"
 	    if ! [[ ${LINE: -5} == ".slha" ]]; then
 			LINE=$LINE.slha
 	    fi
@@ -153,10 +191,11 @@ else
 	    fi
 	    inside_print "[INFO] SUSYHit done. Running CheckMATE..."
 	    $CHECKMATE -n $IT_NAME -pyp "p p > $PYTHIA" -a $ANALYSES -maxev $NEV -slha $WORK_DIR/$LINE -oe $OUTPUT_EXISTS -od $WORK_DIR $PARAMS
-		move_results $IT_NAME
+		move_results $IT_NAME $LINE
 		if ! [[ $USE_FILE_NAMES == 'true' ]]; then 
 			IT=$((IT+1)) 
 		fi
 	done 3<"$FILE"
 fi
+
 inside_print "[END]"
